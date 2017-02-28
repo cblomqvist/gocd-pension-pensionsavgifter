121399adf92be801f7b495d18aed587d4fb0af3f
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-02-28 16:15:41 +0100

    Fix calculation and use rounding

--- calc_alderspensionsavgift.sh
+++ calc_alderspensionsavgift.sh
@@ -1,3 +1,3 @@
-inkomst=$(target/socarb.txt)
-let res="${inkomst} * 0.1021"
+inkomst=$(cat target/socarb.txt)
+res=$(echo "scale=0; (${inkomst} * 0.1021) / 1" | bc -l)
 echo ${res} > target/alderspensionsavgift.txt
