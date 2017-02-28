92995b0328fc60ddf6f19cfde86b4556e577600f
Christer Blomqvist <christer.blomqvist@gmail.com>
2017-02-28 16:20:36 +0100

    Update calc_arbetsgivaravgift_och_egenavgift.sh

--- calc_arbetsgivaravgift_och_egenavgift.sh
+++ calc_arbetsgivaravgift_och_egenavgift.sh
@@ -1,3 +1,3 @@
 inkomst=$(cat target/lon.txt)
-res=$(echo "scale=0; (${inkomst} * 0.07) / 1" | bc -l)
+res=$(echo "scale=0; (${inkomst} * 0.1021) / 1" | bc -l)
 echo ${res} > target/arbetsgivaravgift_och_egenavgift.txt
