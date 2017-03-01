a2ad0f78184139be85e800f2cb2e06b5d3cac4a7
Christer Blomqvist <christer.blomqvist@gmail.com>
2017-03-01 09:01:53 +0100

    Change rate to 4.5%

--- calc_tjanstepension.sh
+++ calc_tjanstepension.sh
@@ -1,3 +1,3 @@
 inkomst=$(cat target/lon.txt)
-res=$(echo "scale=0; (${inkomst} * 0.10) / 1" | bc -l)
+res=$(echo "scale=0; (${inkomst} * 0.045) / 1" | bc -l)
 echo ${res} > target/tjanstepension.txt
