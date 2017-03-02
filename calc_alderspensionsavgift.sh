75a8c2c84a67862a34b5b2028ab013ee5269c8df
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 15:49:48 +0100

    debug output: Hello!

--- calc_alderspensionsavgift.sh
+++ calc_alderspensionsavgift.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+echo Hello!
 mkdir -p output/alderspensionsavgift
 inkomst=$(cat upstream/output/socarb/res.txt)
 res=$(echo "scale=0; (${inkomst} * 0.1021) / 1" | bc -l)
