7537dda028e97bc7ba3841d6ae0712ffeb555e69
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 15:26:15 +0100

    New path in input

--- calc_alderspensionsavgift.sh
+++ calc_alderspensionsavgift.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 mkdir -p output/alderspensionsavgift
-inkomst=$(cat input/socarb/res.txt)
+inkomst=$(cat upstream/output/socarb/res.txt)
 res=$(echo "scale=0; (${inkomst} * 0.1021) / 1" | bc -l)
 echo ${res} > output/alderspensionsavgift/res.txt
