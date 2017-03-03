0364ddb599be3b0877a8adf1da64b26de2ea6e13
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-03 10:13:47 +0100

    Use variable for INPUT

--- calc_allman_pensionsavgift.sh
+++ calc_allman_pensionsavgift.sh
@@ -2,6 +2,6 @@
 set -e
 mkdir -p output
 INPUT=upstream/output
-inkomst=$(cat upstream/output/lon.txt)
+inkomst=$(cat ${INPUT}/lon.txt)
 res=$(echo "scale=0; (${inkomst} * 0.07) / 1" | bc -l)
 echo ${res} > output/allman_pensionsavgift.txt
